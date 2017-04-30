f6773bbe5af05f8eac242cfbc3be81002476c060
Jon Seidel <jseidel@edpci.com>
2017-04-30 05:26:52 -0700

    Initial commit; 'Hello World' example

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+}
+
+  // Entries must resolve to files
+  const commonConfig = {
+  entry: {
+    app: PATHS.app,
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo flailing',
+    }),
+  ],
+}
+module.exports = (env) => {
+  console.log('env', env)
+  return commonConfig
+}
